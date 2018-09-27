363ae4578e43949170df2d82018ade3b603b956e
Mina <mina_gold96@hotmail.com>
2018-09-27 16:14:59 +0200

    eshtaa

--- app/src/main/java/com/example/ahmed/egytour/app/AppConfig.java
+++ app/src/main/java/com/example/ahmed/egytour/app/AppConfig.java
@@ -3,8 +3,8 @@ package com.example.ahmed.egytour.app;
 public class AppConfig {
 
     // Server user login url
-    public static String URL_LOGIN = "http://192.168.1.3/android_login_api/login.php";
+    public static String URL_LOGIN = "http://172.20.10.3/android_login_api/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://192.168.1.3/android_login_api/register.php";
+    public static String URL_REGISTER = "http://172.20.10.3/android_login_api/register.php";
 }
